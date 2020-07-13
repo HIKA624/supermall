bb25b0e5459133eb3a87b34cfdac88e117d03ef0
HIKA624 <515850685@qq.com>
2020-07-13 18:52:54 +0800

    更新

--- vue.config.js
+++ vue.config.js
@@ -10,15 +10,15 @@ module.exports = {
       }
     }
   },
-	config.module
-	.rule("css")
-	.test(/\.css$/)
-	.oneOf("vue")
-	.resourceQuery(/\?vue/)
-	.use("px2rem")
-	.loader("px2rem-loader")
-	.options({
-
-		remUnit: 192
-	})
+	// config:module
+	// .rule("css")
+	// .test(/\.css$/)
+	// .oneOf("vue")
+	// .resourceQuery(/\?vue/)
+	// .use("px2rem")
+	// .loader("px2rem-loader")
+	// .options({
+	//
+	// 	remUnit: 192
+	// })
 }
